65df1ae4655c1a798d00bdeb7babc8a7441137e0
Tom Hsu <thomaschsu@gmail.com>
2019-09-06 10:29:24 -0400

    Added jsonplaceholder api

--- app.js
+++ app.js
@@ -5,45 +5,54 @@ document.querySelector('#get-users').addEventListener('click', getUsers)
 
 document.querySelector('#get-posts').addEventListener('click', getPosts)
 
-function getText () {
+function getText() {
     fetch('sample.txt')
-    .then((res) => {
-        return res.text()
-    })
-    .then((data) => {
-        document.querySelector('#output').innerHTML = data
-    })
-    .catch((err) => {
-        console.log(`Error: ${err}`)
-    })
+        .then((res) => {
+            return res.text()
+        })
+        .then((data) => {
+            document.querySelector('#output').innerHTML = data
+        })
+        .catch((err) => {
+            console.log(`Error: ${err}`)
+        })
 }
 
-function getUsers () {
+function getUsers() {
     fetch('users.json')
-    .then((res) => {
-        return res.json()
-    })
-    .then((data) => {
-        let output = '<h2>Users</h2>'
-        data.forEach((user) => {
-            output += `
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            let output = '<h2>Users</h2>'
+            data.forEach((user) => {
+                output += `
             <ul>
                 <li>ID: ${user.id}</li>
                 <li>Name: ${user.name}</li>
                 <li>Email: ${user.email}</li>
             </ul>
             `
-            document.querySelector('#output').innerHTML = output
+                document.querySelector('#output').innerHTML = output
+            })
         })
-    })
 }
 
 function getPosts() {
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((res) => {
-        return res.json()
-    })
-    .then((data) => {
-        console.log(data)
-    })
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            let output = '<h2>Posts</h2>'
+            data.forEach((post) => {
+                output += `
+            <div>
+                <h3>${post.title}</h3>
+                <p>Name: ${post.body}</p>
+            </div>
+            `
+                document.querySelector('#output').innerHTML = output
+            })
+        })
 }
