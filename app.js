d5fba50bbee4f42546d657faeff1edcb2ec845d3
Tom Hsu <thomaschsu@gmail.com>
2019-09-06 10:55:40 -0400

    Added bootstrap

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ function getUsers() {
             let output = '<h2>Users</h2>'
             data.forEach((user) => {
                 output += `
-            <ul>
-                <li>ID: ${user.id}</li>
-                <li>Name: ${user.name}</li>
-                <li>Email: ${user.email}</li>
+            <ul class="list-group mb-3">
+                <li class="list-group-item">ID: ${user.id}</li>
+                <li class="list-group-item">Name: ${user.name}</li>
+                <li class="list-group-item">Email: ${user.email}</li>
             </ul>
             `
                 document.querySelector('#output').innerHTML = output
@@ -49,7 +49,7 @@ function getPosts() {
             let output = '<h2>Posts</h2>'
             data.forEach((post) => {
                 output += `
-            <div>
+            <div class="card card-body mb-3">
                 <h3>${post.title}</h3>
                 <p>Name: ${post.body}</p>
             </div>
